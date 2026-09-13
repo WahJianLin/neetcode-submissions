2a6aa3c7a7b1b81a154d32c49623fc9e4b858489
WahJianLin <wahjian.lin@gmail.com>
2026-09-13 18:13:30 -0400

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache = {0:0, 1:1, 2:2}
+        def rec(n, cache):
+            if n in cache:
+                return cache[n]
+            val = rec(n-1, cache) + rec(n-2, cache)
+            cache[n] = val
+            return val
+        return rec(n, cache)
